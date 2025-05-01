9145f5d895a12939bb6d51c9cc464e9dbf523ddf
techocea <dunsfordbright@gmail.com>
2025-05-01 13:34:02 +0530

    font fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/Footer";
 const poppins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
   display: "swap",
 });
 
